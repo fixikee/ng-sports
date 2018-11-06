8a246b4bcdd8506b26120df733274a591d539e5b
fixikee <renato.kazinczi@gmail.com>
2018-11-06 23:30:44 +0100

    News feed page #1 :goat:

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {NewsFeedComponent} from './screens/news-feed/news-feed.component';
+import {EventsComponent} from './screens/events/events.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/news', pathMatch: 'full'},
+  {path: 'news', component: NewsFeedComponent},
+  {path: 'events', component: EventsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsFeedComponent } from './screens/news-feed/news-feed.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EventsComponent } from './screens/events/events.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsFeedComponent,
+    EventsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
