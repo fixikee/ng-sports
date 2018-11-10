db07017725172727875f007ba5c5e86325654515
rozsabence <robence@gmail.com>
2018-11-10 19:10:38 +0100

    Login page #2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NewsFeedComponent} from './screens/news-feed/news-feed.component';
 import {EventsComponent} from './screens/events/events.component';
+import {LoginComponent} from './screens/login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/news', pathMatch: 'full'},
   {path: 'news', component: NewsFeedComponent},
   {path: 'events', component: EventsComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NewsFeedComponent } from './screens/news-feed/news-feed.component';
-import { AppRoutingModule } from './app-routing.module';
-import { EventsComponent } from './screens/events/events.component';
+import {AppComponent} from './app.component';
+import {NewsFeedComponent} from './screens/news-feed/news-feed.component';
+import {AppRoutingModule} from './app-routing.module';
+import {EventsComponent} from './screens/events/events.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
+import {LoginComponent} from './screens/login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsFeedComponent,
-    EventsComponent
+    EventsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -24,9 +28,15 @@ import {MatSelectModule} from '@angular/material/select';
     MatToolbarModule,
     MatInputModule,
     MatFormFieldModule,
-    MatSelectModule
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
