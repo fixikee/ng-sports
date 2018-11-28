9df3ce0fd3e577e5b146585ce389ba17251b65b0
rozsabence <robence@gmail.com>
2018-11-28 23:07:53 +0100

    #30 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-sports';
+
+  constructor(
+    public router: Router
+  ) {
+  }
+
+
+  isActive(route: string): string {
+    console.log('----');
+    console.log(this.router.url[route.length]);
+    console.log('----');
+
+    if (route === '/activity') {
+      console.log('---activity---');
+      console.log(this.router.url);
+      console.log(this.router.url[route.length]);
+    }
+
+    return this.router.url.includes(route) &&
+    (route.length === this.router.url.length || this.router.url[route.length] === '/')
+      ? 'active'
+      : '';
+  }
+
 }
